778936d2ca1520b9935fa70e8504228225be9dbd
vetrivel <rameshinkumar627@gmail.com>
2021-03-21 22:03:32 +0530

    co

--- wizard/story_export.py
+++ wizard/story_export.py
@@ -21,22 +21,38 @@ class StoryExport(models.TransientModel):
     name = fields.Char(string="Name")
 
     def trigger_export(self):
-        recs = self.env["story.book"].search([("has_published", "=", True),
-                                              ("is_exported", "=", False),
-                                              ("date_of_publish", "=", datetime.now())])[:100]
-        print(recs, "---")
-        if recs:
-            json_data = self.generate_json(recs)
-            tmp_file = self.generate_tmp_file(json_data)
-            # self.move_tmp_file(tmp_file)
-            tmp_file.close()
+        recs = self.env["story.language"].search([])
+        for rec in recs:
+            # export_list = self.env["story.book"].search([("has_published", "=", True),
+            #                                              ("is_exported", "=", False),
+            #                                              ("date_of_publish", "=", datetime.now())])[:100]
+            export_list = self.env["story.book"].search([("has_published", "=", True)])
 
-    def generate_json(self, recs):
+            if export_list:
+                self.story_export(export_list, rec.name)
+                self.category_export(export_list, rec.name)
+
+    def story_export(self, recs, language):
+        json_data = self.generate_story_json(recs)
+        file_name = "{}_story".format(language)
+        tmp_file = self.generate_tmp_file(json_data, file_name)
+        # self.move_tmp_file(tmp_file)
+        tmp_file.close()
+
+    def category_export(self, recs, language):
+        json_data = self.generate_category_json(recs)
+        file_name = "{}_category".format(language)
+        tmp_file = self.generate_tmp_file(json_data, file_name)
+        # self.move_tmp_file(tmp_file)
+        tmp_file.close()
+
+    def generate_story_json(self, recs):
         book = []
 
         for rec in recs:
             story = {
                 "story_id": rec.id,
+                "published_on": rec.date_of_publish,
                 "name": rec.name,
 
                 "site_url": rec.site_url,
@@ -56,6 +72,19 @@ class StoryExport(models.TransientModel):
 
         return book
 
+    def generate_category_json(self, recs):
+        category = []
+        cat_id = []
+        for rec in recs:
+            for item in rec.tag_ids:
+                if item.name and item.url and (item.id not in cat_id):
+                    category.append({"name": item.name,
+                                     "url": item.url})
+                    cat_id.append(item.id)
+
+        return category
+
+
     def move_tmp_file(self, tmp):
         ssh_client = SSHClient()
         ssh_client.set_missing_host_key_policy(AutoAddPolicy())
@@ -69,9 +98,10 @@ class StoryExport(models.TransientModel):
         sftp_client.put(tmp.name, REMOTE_FILE.format(file_name=file_name))
         sftp_client.close()
 
-    def generate_tmp_file(self, file_data):
+    def generate_tmp_file(self, file_data, file_suffix):
         prefix = datetime.now().strftime('%s')
-        tmp = tempfile.NamedTemporaryFile(prefix=prefix, suffix=".json", delete=False, mode="w+")
+        suffix = "{}.json".format(file_suffix)
+        tmp = tempfile.NamedTemporaryFile(prefix=prefix, suffix=suffix, delete=False, mode="w+")
         json.dump(file_data, tmp)
         tmp.flush()
 
