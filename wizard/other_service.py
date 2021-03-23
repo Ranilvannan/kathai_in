3350013116dc066265809530cbd4599fa2678d3d
vetrivel <rameshinkumar627@gmail.com>
2021-03-23 23:58:24 +0530

    co

--- wizard/other_service.py
+++ wizard/other_service.py
@@ -2,6 +2,8 @@ from odoo import models, fields, api
 from datetime import datetime
 import string
 import random
+from itertools import groupby
+from operator import itemgetter
 
 
 class OtherService(models.Model):
@@ -9,11 +11,8 @@ class OtherService(models.Model):
     _description = "Other Service"
 
     def generate_url(self, text):
-        new_text = text.lower()
-        new_text = new_text.replace(" ", "-")
-        new_text = new_text.replace("'", "")
-        new_text = new_text.replace(",", "")
-
-        res = ''.join(random.choices(string.ascii_lowercase + string.digits, k=7))
+        new_text = "".join([c if c.isalnum() else "-" for c in text.lower()])
+        new_text = "".join(map(next, map(itemgetter(1), groupby(new_text))))
+        res = "".join(random.choices(string.ascii_lowercase + string.digits, k=7))
         site_path = "{0}-{1}".format(new_text, res)
         return site_path
