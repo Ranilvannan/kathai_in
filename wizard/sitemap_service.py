0f44d196fdd6615429ac645ae15e7371aa81b902
vetrivel <rameshinkumar627@gmail.com>
2021-03-26 22:51:16 +0530

    co

--- wizard/sitemap_service.py
+++ wizard/sitemap_service.py
@@ -12,11 +12,10 @@ from xml.dom import minidom
 HOST = config["story_book_export_host"]
 USERNAME = config["story_book_export_username"]
 KEY_FILENAME = config["story_book_export_public_key_filename"]
-REMOTE_FILE = "/home/vetrivel/english/sitemap/{filename}"
 
 MONTH = [("1", "January")]
 YEAR = [("2021", "2021"), ("2022", "2022")]
-SITE = [("project.site1", "Project Site 1")]
+SITE = [("project_site1", "Project Site 1")]
 
 
 class SitemapService(models.TransientModel):
@@ -34,13 +33,29 @@ class SitemapService(models.TransientModel):
         day = calendar.monthrange(int(year_now), int(month_now))
         from_date = "{0}-{1}-{2}".format(year_now, month_now, "01")
         till_date = "{0}-{1}-{2}".format(year_now, month_now, day[1])
-        site_model = self.site
+
+        if self.site == "project_site1":
+            self.project_site1("project.site1", from_date, till_date)
+            self.project_site1_index("project.site1")
+
+    def project_site1_index(self, site_model):
+        filepath = "/home/vetrivel/english/sitemap/{filename}"
+        filename = "sitemap.xml"
+
+        recs = self.env[site_model].home_page_urls()
+        category_page_list = self.env[site_model].category_page_urls()
+        recs.extend(category_page_list)
+        data = self.generate_sitemap_xml_data(recs)
+        self.generate_tmp_file(data, filepath, filename)
+
+    def project_site1(self, site_model, from_date, till_date):
         month_name = datetime.now().strftime("%B").lower()
+        filepath = "/home/vetrivel/english/sitemap/{filename}"
         filename = "{0}_sitemap.xml".format(month_name)
 
         recs = self.env[site_model].story_page_urls(from_date, till_date)
         data = self.generate_sitemap_xml_data(recs)
-        self.generate_tmp_file(data, filename)
+        self.generate_tmp_file(data, filepath, filename)
 
     def generate_sitemap_xml_data(self, recs):
         urlset = Element("urlset", xmlns="http://www.sitemaps.org/schemas/sitemap/0.9")
@@ -58,7 +73,7 @@ class SitemapService(models.TransientModel):
 
         return data
 
-    def move_tmp_file(self, tmp, filename):
+    def move_tmp_file(self, tmp, filepath, filename):
         ssh_client = SSHClient()
         ssh_client.set_missing_host_key_policy(AutoAddPolicy())
 
@@ -67,16 +82,16 @@ class SitemapService(models.TransientModel):
                            key_filename=KEY_FILENAME)
 
         sftp_client = ssh_client.open_sftp()
-        sftp_client.put(tmp.name, REMOTE_FILE.format(filename=filename))
+        sftp_client.put(tmp.name, filepath.format(filename=filename))
         sftp_client.close()
 
-    def generate_tmp_file(self, file_data, filename):
+    def generate_tmp_file(self, file_data, filepath, filename):
         prefix = datetime.now().strftime('%s')
         with tempfile.NamedTemporaryFile(prefix=prefix, suffix=".xml") as tmp:
             data = str.encode(file_data)
             tmp.write(data)
             tmp.flush()
-            self.move_tmp_file(tmp, filename)
+            self.move_tmp_file(tmp, filepath, filename)
 
     def trigger_monthly_sitemap(self):
         pass
