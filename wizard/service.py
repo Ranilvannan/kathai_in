cc0f83c108e4fc55289548e54f23b02a5a981010
vetrivel <rameshinkumar627@gmail.com>
2021-03-27 22:54:00 +0530

    co

--- wizard/service.py
+++ wizard/service.py
@@ -5,23 +5,23 @@ class Service(models.TransientModel):
     _name = "service.service"
     _description = "Service Service"
 
-    def trigger_crawl(self):
+    def trigger_desitales2_crawl(self):
         self.env["crawl.service"].trigger_desitales2_crawl()
 
-    def trigger_history_crawl(self):
+    def trigger_desitales2_history_crawl(self):
         self.env["crawl.service"].trigger_desitales2_history_crawl()
 
-    def trigger_category_tag_update(self):
+    def trigger_book_category_tag_update(self):
         self.env["category.tag"].trigger_generate_tag()
 
-    def trigger_site1_import(self):
+    def trigger_project_site1_import(self):
         self.env["project.site1"].trigger_data_import()
 
-    def trigger_site_data_update(self):
+    def trigger_project_site1_data_update(self):
         self.env["project.site1"].trigger_site_data()
 
-    def trigger_site_validate(self):
+    def trigger_project_site1_validate(self):
         self.env["project.site1"].trigger_check_valid()
 
-    def trigger_export(self):
+    def trigger_project_site1_export(self):
         self.env["project.site1"].trigger_export()
