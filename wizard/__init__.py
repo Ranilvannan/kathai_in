bd96b8705cf4fc57c2966840018fb7af9322613f
rameshkumar <rameshinkumar627@gmail.com>
2021-04-04 18:41:40 +0530

    Lap 1

--- wizard/__init__.py
+++ wizard/__init__.py
@@ -1,5 +1,5 @@
 from . import crawl_service
-from . import insert_service
+from . import transfer_service
 from . import site_update_service
 from . import export_service
 from . import reset_service
